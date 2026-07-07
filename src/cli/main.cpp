42e7dbd3b5bd0bbf0a6f3a8aeec5546a41f470db
devin <127160473+devin-thomas@users.noreply.github.com>
2026-07-07 01:02:27 -0500

    Restore typed RHS auto style in CLI main

--- src/cli/main.cpp
+++ src/cli/main.cpp
@@ -9,13 +9,13 @@
 using namespace vidchopper;
 
 auto main(int argc, char* argv[]) -> int {
-    std::vector<std::string> arguments {};
+    auto arguments = std::vector<std::string> {};
     arguments.reserve(static_cast<std::size_t>(argc > 1 ? argc - 1 : 0));
     for (int index = 1; index < argc; ++index) {
         arguments.emplace_back(argv[index]);
     }
 
-    const CliRunRequest request {
+    const auto request = CliRunRequest {
         .arguments = std::move(arguments),
         .executable_path = argc > 0 ? Path {argv[0]} : Path {},
         .output = std::cout,
